21a37ee43c7661d5ef43177fcaff143e1ab9abfd
AryanRaj1330 <24je0890@iitism.ac.in>
2025-05-03 00:56:16 +0530

    next questions feature added

--- src/components/quiz/quiz.jsx
+++ src/components/quiz/quiz.jsx
@@ -3,13 +3,29 @@ import "./quiz.css"
 
 const Quiz=()=>{
 
+    const[setting,setSetting]= useState(false)
+    const[amount,setAmount]= useState(5)
+    const[difficulty,setDifficulty]= useState("")
+    const[category,setCategory]= useState("")
+    const[type,setType]= useState("")
+    const[questionIndex, setQuestionIndex]= useState(0)
     const[questions,setquestions]= useState([])
-    const[isLoading,setIsLoading]= useState(true)
+    const[isLoading,setIsLoading]= useState(false)
+    const[selectedOption,setSelectedOption]= useState(null)
+    const[isCorrect,setIsCorrect]= useState(null)
+    const[score,setScore]= useState(0)
+    const[quizCompleted,setQuizCompleted]= useState(false)
 
     useEffect(()=>{
+        if(!setting) return
+
         async function quizData(){
             try{
-                const response= await fetch(new Request("https://api.allorigins.win/raw?url=" + encodeURIComponent("https://opentdb.com/api.php?amount=10&category=22&difficulty=easy&type=multiple")))
+                let url=`https://opentdb.com/api.php?amount=${amount}`
+                if(category) url+=`&category=${category}`
+                if(difficulty) url+=`&difficulty=${difficulty}`
+                if(type) url+=`&type=${type}`
+                const response= await fetch(new Request(url))
                 
                 const data= await response.json()
                 setquestions(data.results)
@@ -22,36 +38,190 @@ const Quiz=()=>{
         }
         
         quizData()
-    },[])
+    },[setting])
+
+    const optionSelected=(option)=>{
+        setSelectedOption(option)
+        setIsCorrect(option===questions[questionIndex].correct_answer)
+        if(option===questions[questionIndex].correct_answer){
+            setScore(prev=>prev+1)
+        }
+    }
+
+    const nextButton=()=>{
+        if(questionIndex<questions.length-1){
+            setQuestionIndex(prev=>prev+1)
+            setSelectedOption(null)
+            setIsCorrect(null)
+        }
+        else{
+            setQuizCompleted(true)
+        }
+    }
 
     if(isLoading){
         return(
             <h2>Loading...</h2>
         )
     }
+
+    const message=()=>{
+        const percentage=(score/questions.length)*100
+        if(percentage<=30) return "Better Luck Next Time! You need a lot Practice."
+        else if(percentage>30&&percentage<=70) return "Not Bad, But practice More."
+        else if(percentage>70&&percentage<100) return "Well Done!"
+        else return "Hooray!! You have done it."
+    }
+
+    if(quizCompleted){
+        return(
+            <div id="container">
+                <h2>
+                    Quiz Completed!
+                </h2>
+                <p> Your Score: {score}/{questions.length}</p>
+                <p> {message()}</p>
+
+            </div>
+        )
+    }
+
     return(
         <div id="container">
-            <div id="track">1/5</div>
-            <div id="question">
-                {questions.length>0?(
-                    <h2 dangerouslySetInnerHTML={{__html:questions[0].question}} />
-                ):(
-                    <h2>Loading...</h2>                    
-                )}
-            </div>
-            <div id="option-container">
+      {!setting ? (
+        <>
+          <h2>Customize your Quiz.</h2>
+          <form
+            onSubmit={(event) => {
+              event.preventDefault();
+              setQuestionIndex(0);
+              setScore(0);
+              setSelectedOption(null);
+              setIsCorrect(null);
+              setQuizCompleted(false);
+              setIsLoading(true);
+              setSetting(true);
+            }}
+          >
+            <label>
+              Number of Questions:{" "}
+              <input
+                type="number"
+                min="1"
+                max="20"
+                value={amount}
+                onChange={(e) => setAmount(Number(e.target.value))}
+              />
+            </label>
+            <br />
+            <label>
+              Difficulty:
+              <select
+                value={difficulty}
+                onChange={(e) => setDifficulty(e.target.value)}
+              >
+                <option value="">Any Difficulty</option>
+                <option value="easy">Easy</option>
+                <option value="medium">Medium</option>
+                <option value="hard">Hard</option>
+              </select>
+            </label>
+            <br />
+            <label>
+              Category:
+              <select
+                value={category}
+                onChange={(e) => setCategory(e.target.value)}
+              >
+                <option value="">Any Category</option>
+                <option value="9">General Knowledge</option>
+                <option value="10">Entertainment: Books</option>
+                <option value="11">Entertainment: Films</option>
+                <option value="12">Entertainment: Music</option>
+                <option value="13">Entertainment: Musicals & Theatres</option>
+                <option value="14">Entertainment: Television</option>
+                <option value="15">Entertainment: Video Games</option>
+                <option value="16">Entertainment: Board Games</option>
+                <option value="29">Entertainment: Comics</option>
+                <option value="31">Entertainment: Japanese Anime & Manga</option>
+                <option value="32">Entertainment: Cartoons & Animations</option>
+                <option value="17">Science & Nature</option>
+                <option value="18">Science: Computers</option>
+                <option value="19">Science: Mathematics</option>
+                <option value="30">Science: Gadgets</option>
+                <option value="20">Mythology</option>
+                <option value="21">Sports</option>
+                <option value="22">Geography</option>
+                <option value="23">History</option>
+                <option value="24">Politics</option>
+                <option value="25">Art</option>
+                <option value="26">Celebrities</option>
+                <option value="27">Animals</option>
+                <option value="28">Vehicles</option>
+              </select>
+            </label>
+            <br />
+            <label>
+              Type:
+              <select value={type} onChange={(e) => setType(e.target.value)}>
+                <option value="">Any Type</option>
+                <option value="multiple">Multiple</option>
+                <option value="boolean">True/False</option>
+              </select>
+            </label>
+            <br />
+            <button type="submit">Ready</button>
+          </form>
+        </>
+      ) : isLoading ? (
+        <h2>Loading...</h2>
+      ) : quizCompleted ? (
+        <>
+          <h2>Quiz Completed!</h2>
+          <p>Your Score: {score}/{questions.length}</p>
+          <p>{message()}</p>
+        </>
+      ) : (
+        <>
+          <div id="track">{questionIndex + 1}/{questions.length}</div>
+          <div id="question">
+            <h2
+              dangerouslySetInnerHTML={{
+                __html: questions[questionIndex].question,
+              }}
+            />
+          </div>
+          <div id="option-container">
             <ul>
-                {questions.length>0&&
-                    [...questions[0].incorrect_answers,questions[0].correct_answer]
-                    .sort(()=> Math.random()-0.5)
-                    .map((option,idx)=>(
-                        <li key={idx} className="options" dangerouslySetInnerHTML={{__html:option}} />
-                    ))
-                }
+              {[...questions[questionIndex].incorrect_answers, questions[questionIndex].correct_answer]
+                .sort(() => Math.random() - 0.5)
+                .map((option, idx) => {
+                  let className = "options";
+                  if (selectedOption) {
+                    if (option === selectedOption) {
+                      className +=
+                        option === questions[questionIndex].correct_answer
+                          ? " correct"
+                          : " incorrect";
+                    }
+                  }
+                  return (
+                    <li
+                      key={idx}
+                      className={className}
+                      dangerouslySetInnerHTML={{ __html: option }}
+                      onClick={() => optionSelected(option)}
+                    />
+                  );
+                })}
             </ul>
-            </div>
-            <button>Next</button>
-        </div>
+          </div>
+          <button onClick={nextButton} disabled={!selectedOption}>
+            Next
+          </button>
+        </>
+      )}
+    </div>
     )
 }
 
